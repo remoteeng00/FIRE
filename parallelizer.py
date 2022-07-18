715fcaa87ba374f4648cec916b7d281484c2076d
remoteeng00 <67749546+remoteeng00@users.noreply.github.com>
2022-07-18 14:37:50 +0700

    Update parallelizer.py

--- parallelizer.py
+++ parallelizer.py
@@ -1,6 +1,6 @@
-import glob, os
-
-def jobs(self):
+def new_jobs(self):
+    import glob, os
+    
     files = glob.glob(os.path.join(self.data_folder,"*.txt"))
     #self.logger.info("Parallelizing per file: %s" % files)
     return [(f,) for f in files]
